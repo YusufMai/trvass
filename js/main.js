26b29a5fef5a722c9ee1dea0442681a1c055d5cd
Yusuf Abubakar Mai <maiyusuf305@gmail.com>
2026-05-12 11:02:21 +0100

    Fresh Upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 (function ($) {
     "use strict";
 
-    // Spinner
+    // 1. Spinner Logic
     var spinner = function () {
         setTimeout(function () {
             if ($('#spinner').length > 0) {
@@ -10,156 +9,89 @@
             }
         }, 1);
     };
-    spinner(0);
+    spinner();
     
-    
-    // Initiate the wowjs
+    // 2. Initiate wowjs (Animations)
     new WOW().init();
 
-
-    // Sticky Navbar
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-        $('.sticky-top').addClass('shadow-sm');
-    } else {
-        $('.sticky-top').removeClass('shadow-sm');
-    }
-});
-
-
-    // Hero Header carousel
-    $(".header-carousel").owlCarousel({
-        animateOut: 'slideOutDown',
-        items: 1,
-        autoplay: true,
-        smartSpeed: 500,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-down"></i>'
-        ],
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 5,
-        time: 2000
-    });
-
-    $(".team-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1500,
-    dots: true,
-    loop: true,
-    center: true,
-    responsive: {
-        0:{ items:1 },
-        576:{ items:1 },
-        768:{ items:2 },
-        992:{ items:3 }
-    }
-});
-
-
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-})(jQuery);
-
-=======
-(function ($) {
-    "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
-
-
-    // Sticky Navbar
+    // 3. Sticky Navbar Logic
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.sticky-top').addClass('shadow-sm').css('top', '0px');
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            // FIXED: Removed the -100px top offset so it doesn't hide at the top
+            $('.sticky-top').removeClass('shadow-sm').css('top', '0px');
         }
     });
 
-
-    // Hero Header carousel
+    // 4. Hero Header Carousel
     $(".header-carousel").owlCarousel({
-        animateOut: 'slideOutDown',
         items: 1,
         autoplay: true,
-        smartSpeed: 500,
-        dots: false,
+        smartSpeed: 1000,
+        dots: true,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-down"></i>'
-        ],
+            '<i class="bi bi-arrow-right"></i>'
+        ]
     });
 
-
-    // Facts counter
+    // 5. Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 5,
         time: 2000
     });
 
+    // 6. Team Carousel
     $(".team-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1500,
-    dots: true,
-    loop: true,
-    center: true,
-    responsive: {
-        0:{ items:1 },
-        576:{ items:1 },
-        768:{ items:2 },
-        992:{ items:3 }
-    }
-});
-
-
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: true,
+        loop: true,
+        margin: 25,
+        responsive: {
+            0:{ items:1 },
+            576:{ items:1 },
+            768:{ items:2 },
+            992:{ items:3 }
+        }
     });
+
+    // 7. Testimonial Carousel (Added for completeness)
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: true,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText: [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{ items:1 },
+            768:{ items:2 },
+            992:{ items:3 }
+        }
+    });
+
+    // 8. Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
 
-
 })(jQuery);
-
->>>>>>> 3bfb296d38cb3359754bee98328ea1f5b35adb2e
